4adcfa0940d018086e5c380d3a334db754d45f7b
yosef khayat <yousef.khayat24@gmail.com>
2019-05-15 16:24:05 +0300

    update colors and logo

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -4,7 +4,7 @@ import { Navbar,NavItem,Dropdown ,Divider} from 'react-materialize'
 
 const NavBar = ()=>{
     return(
-         <Navbar fixed className="NavBar-tmp" brand={<a />} alignLinks="right">
+         <Navbar fixed className="NavBar-tmp" brand={<a>TranScan</a>} alignLinks="right">
                 <NavItem href="">
                 Getting started
                 </NavItem>
