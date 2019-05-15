ed51ea4b80a72ae2957bf619c9d5adaa3b209122
yosef khayat <yousef.khayat24@gmail.com>
2019-05-15 13:59:15 +0300

    Add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import SignUp from './components/auth/SignUp'
+import NavBar from './components/layouts/NavBar'
+import './App.css'
 
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <NavBar></NavBar>
+
     </div>
   );
 }
---
+++ src/components/layouts/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Navbar,NavItem,Dropdown ,Divider} from 'react-materialize'
+
+
+const NavBar = ()=>{
+    return(
+         <Navbar brand={<a />} alignLinks="right">
+                <NavItem href="">
+                Getting started
+                </NavItem>
+                <NavItem href="">
+                Components
+                </NavItem>
+                <Dropdown  trigger={<a>profile</a>}>
+                <a href="#">
+                one
+                </a>
+                <a href="#">
+                two
+                </a>
+                <Divider/>
+                <a href="#">
+                three
+                </a>
+                </Dropdown>
+            </Navbar>
+    )
+}
+export default NavBar
