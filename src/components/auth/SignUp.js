51c65def636b20a7d9dac5b8103e427600c789f8
yosef <yousef.khayat24@gmail.com>
2019-05-20 15:03:53 +0300

    update new signup

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react'
-import { Button} from 'react-materialize'
-
+import { Button  , TextInput,Checkbox ,Icon ,Parallax,Collapsible,CollapsibleItem} from 'react-materialize'
+import cover1 from '../../img/cover1.jpg'
 
 
 
@@ -8,15 +8,47 @@ class SignUp extends Component{
     
     render(){
         return (
-            <div className="SignUp">
+            <div id="SignUp">
 
            
                 <div className="intro" >
 
                     <div className="intro-content">
-                        <Button large className="grey darken-3 btn-intro" >More Info</Button>
-                        <Button large className="grey darken-3 btn-intro">Sign Up</Button>
+                        <Button large className="grey darken-3 btn-intro" href="#info" node='a'>More Info</Button>
+                        <Button large className="grey darken-3 btn-intro" href="#join" node='a'>Join Us</Button>
                     </div> 
+                    
+                </div>
+                <div id="info">
+                        jfsdabb
+                        
+                        <br>
+                        </br>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </div>
+                <div>
+                </div>
+                <div id="join">
+                    <div id="touch">
+                    <h2 >Sign Up</h2> 
+                    </div>
+                    <form action="" id="contact">
+                    <TextInput label="First Name" icon={<Icon>contacts</Icon>} />
+                    <TextInput required label="Last Name" icon={<Icon>contacts</Icon>}/>
+                    <TextInput email validate label="Email" icon={<Icon >email</Icon>} />
+                    <TextInput password label="Password" icon={<Icon>security</Icon>}/>
+                    <Checkbox value="have" label="I have a vehicle!" />
+                    <br/>
+                    <Button large className="grey darken-3 btn-intro" >Submit</Button>
+
+                    </form>
                 </div>
             </div>
         )
