1b9956aeac747310378dc8548dc4c237fa85ebf1
IdanR <eidanroas@gmail.com>
2019-10-20 14:40:19 +0300

    resulr obj

---
+++ backend/src/main/java/com/transcan/backendtranscan/domain/ResultMatchObj.java
@@ -0,0 +1,78 @@
+package com.transcan.backendtranscan.domain;
+
+import com.transcan.backendtranscan.services.MapService;
+import com.transcan.backendtranscan.services.RideSuggestionService;
+import com.transcan.backendtranscan.services.SearchRideService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ResultMatchObj {
+    static final int TEXI = 0;
+    static final int PRIVATE_CAR = 1;
+    static final int TEXI6SEAT = 2;
+    static final int MINIBUS = 3;
+    static final int BUS = 4;
+
+    private long mId;
+    private ArrayList<Long> mPassngersIdList;
+    private int mVehicle;
+
+
+    public ResultMatchObj(long id) {
+        this.mId = id;
+        mPassngersIdList = new ArrayList();
+        mVehicle = TEXI;
+    }
+
+    public void addNewPassanger(long id) {
+        mPassngersIdList.add(id);
+    }
+
+    public int getPassengerNum() {
+        return mPassngersIdList.size();
+    }
+
+    public ArrayList<Long> getPassengerList() {
+        return mPassngersIdList;
+    }
+
+    public int VehicleType() {
+        if (getPassengerNum() <= 4)
+            mVehicle = TEXI;
+        else if (getPassengerNum() <= 8)
+            mVehicle = TEXI6SEAT;
+        else if (getPassengerNum() <= 15)
+            mVehicle = MINIBUS;
+        else
+            mVehicle = BUS;
+
+        return mVehicle;
+    }
+
+    public ArrayList<ResultMatchObj> getMatchList(Iterable<RideSearch> rideSearch) {
+
+        ArrayList<ResultMatchObj> arrayList = new ArrayList<ResultMatchObj>();
+        for (RideSearch current : rideSearch) {
+            for (RideSearch passanger : rideSearch) {
+                if (current.getSearchId() != passanger.getSearchId()) {
+                    arrayList.add(new ResultMatchObj(current.getSearchId()));
+                    if (MapService.getDistanceGeoLocation(current.getDesLatLng(), passanger.getDesLatLng()) <= 500 &&
+                            MapService.getDistanceGeoLocation(current.getLocLatLng(), passanger.getLocLatLng()) <= 500) {
+                        arrayList.get(arrayList.size() - 1).addNewPassanger(passanger.getSearchId());
+                    }
+                }
+            }
+
+        }
+        return arrayList;
+    }
+
+
+
+
+
+}
+
+
--- backend/src/main/java/com/transcan/backendtranscan/services/MapService.java
+++ backend/src/main/java/com/transcan/backendtranscan/services/MapService.java
@@ -6,11 +6,16 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
-import com.google.maps.model.LatLng;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MapService {
+
+    @Autowired
+    private SearchRideService searchRideService;
 
     public static String getGeolocation(String address) throws InterruptedException, ApiException, IOException {
         GeoApiContext context = new GeoApiContext.Builder()
@@ -19,15 +24,54 @@ public class MapService {
         GeocodingResult[] results = GeocodingApi.geocode(context,
                 address).await();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        return(results[0].geometry.location.toString());
+        return (results[0].geometry.location.toString());
     }
 
-    public static double getDistanceGeoLocation(){
+    public static float getDistanceGeoLocation(String latLng1, String latLng2) {
 
-        return 500.0;
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(getLat(latLng2) - getLat(latLng1));
+        double dLng = Math.toRadians(getLng(latLng2) - getLng(latLng1));
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(getLat(latLng1))) * Math.cos(Math.toRadians(getLat(latLng2))) *
+                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        float dist = (float) (earthRadius * c);
+
+        return dist;
 
     }
 
+    public static double getLng(String latLng) {
+        return Double.parseDouble(latLng.substring(12));
+    }
 
+    public static double getLat(String latLng) {
+        return Double.parseDouble(latLng.substring(0, 11));
+    }
 
+    public String getMiddlePoint(ArrayList<Long> idList, boolean isLocationPoint) {
+        double latSum = 0;
+        double lngSum = 0;
+
+            for (int i = 0; i < idList.size(); i++) {
+                double lat, lng;
+                if (isLocationPoint) {
+                     lat = getLat(searchRideService.findById(idList.get(i)).orElse(null).getLocLatLng());
+                     lng = getLng(searchRideService.findById(idList.get(i)).orElse(null).getLocLatLng());
+                } else {
+                     lat = getLat(searchRideService.findById(idList.get(i)).orElse(null).getDesLatLng());
+                     lng = getLng(searchRideService.findById(idList.get(i)).orElse(null).getDesLatLng());
+                }
+                latSum +=lat;
+                lngSum += lng;
+            }
+
+        latSum = latSum / idList.size();
+        lngSum = lngSum / idList.size();
+
+        return latSum + "," + lngSum;
+    }
 }
+
+
