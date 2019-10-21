929a1c715f73bd961f8df94f85cba82fbaa36c63
yosef khayat <yousef.khayat24@gmail.com>
2019-10-22 02:16:31 +0300

    udate save userid

--- frontend/src/TranscanApi.js
+++ frontend/src/TranscanApi.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = "http://localhost:5000/api"
 
 // Generate a unique token for storing your bookshelf data on the backend server.
+let username = localStorage.userid1
 let token = localStorage.token
 if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8)
@@ -22,18 +23,18 @@ export  const isLogin = (query) =>
     export  const searchSubmit = (query) =>
     axios.post(`${api}/form_submit/searchsubmit`,query, {
       params: {
-        userId: 2
+        userId: `${username}`
       }} );
 
     
 export  const driverSubmit = (query ) =>
 axios.post(`${api}/form_submit/suggestionsubmit`,query, {
   params: {
-    userId: 1
+    userId: `${username}`
   }} );
 
   export  const findBestMatch =  () =>
   axios.post(`${api}/form_submit/getbestride`,{}, {
     params: {
-      userId: 3
+      userId: `${username}`
     }} );
--- frontend/src/store/actions/AuthActions.js
+++ frontend/src/store/actions/AuthActions.js
@@ -20,7 +20,8 @@ export const signIn = (user, history) => {
     return (dispatch) => {          
       login(user).then(response => {
         if(response.data){
-          LocalStorageService.saveToLocal("transanUserID", response.data.accessToken);
+        LocalStorageService.saveToLocal("transanUserID", response.data.accessToken);
+        LocalStorageService.saveToLocal("userid1", response.data.id);
         dispatch({ type: 'LOGIN_SUCCESS' });
         console.log(response.data.accessToken);
         console.log("InLocalStorage: "+ LocalStorageService.getFromLocal("transanUserID"));
